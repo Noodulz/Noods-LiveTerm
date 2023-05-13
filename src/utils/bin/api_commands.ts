7912abd5cf696098e5cbf996120671546b9f6b09
Jocelyn <28810573+Noodulz@users.noreply.github.com>
2023-05-13 01:27:15 -0400

    Update api_commands.ts

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,7 +1,6 @@
 // // List of commands that require API calls
 
 import { getProjects } from '../api';
-import { getQuote } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
 
